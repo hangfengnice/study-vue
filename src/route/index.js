74a61e5d1e099be6bdf2b207cd4023344077aecb
hangfengnice <nicehangfeng@foxmail.com>
2019-07-18 15:51:40 +0800

    router

--- src/route/index.js
+++ src/route/index.js
@@ -1,11 +1,23 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import route from './router'
+import Vue from "vue";
+import Router from "vue-router";
+import route from "./router";
 
-Vue.use(Router)
+Vue.use(Router);
 
-export default new Router({
-  mode: 'history',
+const router = new Router({
+  mode: "history",
   base: process.env.BASE_URL,
   route
-})
+});
+
+const has_login = false;
+
+router.beforeEach((to, from, next) => {
+  if (to.path == "/login") {
+    next();
+  } else {
+    has_login ? next() : next("/login");
+  }
+});
+
+export default router;
