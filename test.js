836e9b476bbd78c5ea4d9f113002338e10c27856
hangfeng <nicehangfeng@foxmail.com>
2020-11-01 15:12:56 +0800

    feat

---
+++ test.js
@@ -0,0 +1,30 @@
+var islandPerimeter = function (grid) {
+  const dx = [0, 1, 0, -1]
+  const dy = [1, 0, -1, 0]
+  const n = grid.length,
+    m = grid[0].length
+  let ans = 0
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < m; j++) {
+      if (grid[i][j]) {
+        for (let k = 0; k < 4; k++) {
+          let tx = i + dx[k]
+          let ty = j + dy[k]
+          let cnt = 0
+          if (tx < 0 || tx >= n || ty < 0 || ty >= m || !grid[tx][ty]) {
+            cnt += 1
+          }
+          ans += cnt
+        }
+      }
+    }
+  }
+  return ans
+}
+
+let result = islandPerimeter([[0,1,0,0],
+  [1,1,1,0],
+  [0,1,0,0],
+  [1,1,0,0]])
+
+  console.log(result);
