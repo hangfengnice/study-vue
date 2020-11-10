c8c4cadcc8305ae68e7d7b6458bc802a7e58b6f5
hangfeng <nicehangfeng@foxmail.com>
2020-11-10 19:04:46 +0800

    refactor

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,37 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
   extends: [
     'plugin:vue/vue3-essential',
-    '@vue/standard'
+    '@vue/airbnb',
+    '@vue/typescript/recommended',
   ],
   parserOptions: {
-    parser: 'babel-eslint'
+    ecmaVersion: 2020,
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
-}
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'semi': 'off',
+    'func-names': 'off',
+    'no-restricted-syntax': 'off',
+    'no-plusplus': 'off',
+    'prefer-const': 'off',
+    'no-unused-vars': 'off',
+    'no-var': 'off',
+    'global-require': 'off',
+    'eqeqeq': 'off',
+    'prefer-spread': 'off',
+    'no-underscore-dangle': 'off',
+    'max-len': 'off',
+    'no-unused-expressions': 'off',
+    'consistent-return': 'off',
+    'no-return-assign': 'off',
+    'no-param-reassign': 'off',
+    'prefer-destructuring': 'off',
+    'no-new-func': 'off',
+    'guard-for-in': 'off'
+  },
+};
--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <model></model>
   </div>
-  <router-view/>
 </template>
+<script lang="ts">
+import Model from './components/model.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Model,
+  },
+}
+</script>
 
 <style lang="scss">
 #app {
